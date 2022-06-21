c252bc0a726f53fc306971e8c4eb0f495a3a2dac
Azhaan Ali <azhaanali@live.com>
2022-06-21 07:42:13 -0700

    Reversed integer component bug fixed.

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 # Converts an integer to another base.
 def intComponent(num, base, curr):
-    if num >= 1:
+    if num > 1:
         curr += str(num % base)
         return intComponent(num // base, base, curr)
     return curr
 
 
 # Converts the decimal part of a number to another base.
-def decComponent(num, base, curr, digits):
+def decComponent(num, base, curr, digits=100):
     if not digits == 0 and not num == 0:
         num *= base
         curr += str(int(num))
         return decComponent(num % 1, base, curr, digits - 1)
-    return curr
+    return curr if len(curr) > 0 else "0"
 
 
 # Converts a number to a base.
-def decimalToBase(num, base, digits):
+def decimalToBase(num, base):
     halves = [x for x in str(num).split(".")]
-    answer = intComponent(int(halves[0]), base, "") + "."
-    answer += decComponent(float("." + halves[1]), base, "", digits)
-    return float(answer)
+    answer = intComponent(int(halves[0]), base, "")[::-1] + "."
+    answer += decComponent(float("." + halves[1]), base, "")
+    return answer
 
 
 # Returns the index of the decimal point as well as the number formatted without it.
@@ -45,9 +45,14 @@ def baseToDecimal(num, base):
 
 
 # Converts a number in a given base to another base.
-def convert(num, startBase, endBase, digits=100):
+def convert(num, startBase, endBase):
     num = baseToDecimal(num, startBase)
-    return decimalToBase(num, endBase, digits)
+    return decimalToBase(num, endBase)
 
 
-print(convert(193827, 10, 2))
+answer = convert(
+    124.567,  # The number to convert
+    10,  # The starting base of the number.
+    2  # The ending base of the number.
+)
+print(answer)
